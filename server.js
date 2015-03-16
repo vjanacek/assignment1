9fc64e6dc8fdbb0915d08e2f5252055a9cbe6e66
Victoria Janacek <vjanacek@users.noreply.github.com>
2015-03-16 12:26:45 -0700

    first commit

--- server.js
+++ server.js
@@ -1,44 +1,89 @@
 var express = require("express"),
-app = express(),
-bodyParser = require('body-parser'),
-errorHandler = require('errorhandler'),
-methodOverride = require('method-override'),
-hostname = process.env.HOSTNAME || 'localhost',
-port = parseInt(process.env.PORT, 10) || 4567;
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    hostname = process.env.HOSTNAME || 'localhost',
+    port = parseInt(process.env.PORT, 10) || 4567;
 
 app.get("/", function (req, res) {
-	res.redirect("/index.html");
+  res.redirect("/index.html");
 });
+
+var db = require('mongoskin').db('mongodb://user:password@localhost:27017/todo');
+console.log(db);
 
 var todos = [];
 
 app.get("/addtodo", function (req, res) {
-	var x = req.query.newtodo;
-	todos[todos.length] = x;
-	res.end("added");
-});
+	var x = req.query;
+	var callback = function(error, result){
+		if(result)
+		{
+			res.end("added");
+		}
+	}
+	db.collection("todo").insert(x, callback);
+ });
 
+ app.get("/edittodo", function (req, res) {
+ 	var x = req.query;
+ 	var callback = function(error, result){
+ 		if(result)
+ 		{
+ 			res.end("done");
+ 		}
+ 	}
+	
+	db.collection("todo").findOne({todoid: x.todoid}, function(err, result1) {
+		if(result1){
+			console.log(result1);
+			result1.newtodo = x.newtodo;
+			db.collection("todo").save(result1, callback);
+		}
+		else{
+			db.collection("todo").insert(x, callback);
+		}
+	});
+	
+  });
+
+  
+  
 
 app.get("/deletetodo", function (req, res) {
-	var del = req.query.deleteTodo.index;
-	todos.splice(del, 1);
-	console.log(todos);
-	res.end("deleted");
+	var index = req.query.index;
+	var callback = function(error, result){
+		if(result)
+		{
+			res.end("deleted");
+		}
+	}
+	db.collection("todo").remove({"todoid": index.toString()}, callback);
 });
 
+
 app.get("/listtodos", function (req, res) {
-	res.end(JSON.stringify(todos));
+	db.collection("todo").find().toArray(function(err, result) {
+    	if (result)
+    	{
+			res.end(JSON.stringify(result));
+		}
+	});
 });
+
+
+
 
 app.use(methodOverride());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 app.use(express.static(__dirname + '/client'));
 app.use(errorHandler({
-	dumpExceptions: true,
-	showStack: true
+  dumpExceptions: true,
+  showStack: true
 }));
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
